15c42db0a86441ada6b0a4cc3d94851822f49cac
Danndy6 <87765948+Danndy6@users.noreply.github.com>
2021-10-07 13:58:43 +0000

    Deploying to gh-pages from @ Danndy6/project-group-8@1984c33f3dbbd407f459fe6f8b132c9a0fefc79d 🚀

--- src.e280c6b8.js
+++ src.e280c6b8.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/project-group-8/src.61f87c08.js.map
+//# sourceMappingURL=/project-group-8/src.e280c6b8.js.map
