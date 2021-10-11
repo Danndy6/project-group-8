3e5956b5df4b23d3f107a4233c836041f31c77ff
Oleksandr Vaskevych <oleksandrvaskevich@gmail.com>
2021-10-11 16:06:20 +0300

    final read more

--- src/js/read-more.js
+++ src/js/read-more.js
@@ -1,9 +1,9 @@
-// let products__button = document.querySelectorAll('.products__button');
-// for (let i = 0; i<products__button.length; i++) {
-//     products__button[i].addEventListener('click', function(){
-//         products__button[i].parentNode.classList.toggle('active')
-//     })
-// }
+let products__button = document.querySelectorAll('.products__button');
+for (let i = 0; i<products__button.length; i++) {
+    products__button[i].addEventListener('click', function(){
+        products__button[i].parentNode.classList.toggle('active')
+    })
+}
 
 // function readMore (){
 //     var dots = document.getElementById("dots");
