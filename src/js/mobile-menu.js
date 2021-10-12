d545a40ac8e696105698ad783c9ab10dc428db78
AKdimka <88268598+AKdimka@users.noreply.github.com>
2021-10-12 20:47:57 +0300

    bug-fix-scroll

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -10,10 +10,10 @@
 		openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
 		mobileMenu.classList.toggle('is-open');
 
-		const scrollLockMethod = !isMenuOpen
+	/* 	const scrollLockMethod = !isMenuOpen
 			? 'disableBodyScroll'
 			: 'enableBodyScroll';
-		bodyScrollLock[scrollLockMethod](document.body);
+		bodyScrollLock[scrollLockMethod](document.body) */;
 	};
 	links.forEach(link => {
 		link.addEventListener('click', toggleMenu)
@@ -24,10 +24,10 @@
 
 	// Закрываем мобильное меню на более широких экранах
 	// в случае изменения ориентации устройства.
-	window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+/* 	window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
 		if (!e.matches) return;
 		mobileMenu.classList.remove('is-open');
 		openMenuBtn.setAttribute('aria-expanded', false);
 		bodyScrollLock.enableBodyScroll(document.body);
-	});
+	}) */;
 })();
