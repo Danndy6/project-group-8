6e2fc4d61dcf7ec00fb3c65967aeab2fca7403ca
AKdimka <88268598+AKdimka@users.noreply.github.com>
2021-10-12 13:39:08 +0300

    fix

--- src/js/modal.js
+++ src/js/modal.js
@@ -28,17 +28,3 @@
 	}
 })();
 
-(() => {
-	const refs = {
-		openModalBtn: document.querySelector('[buy-modal-open]'),
-		closeModalBtn: document.querySelector('[buy-modal-close]'),
-		modal: document.querySelector('[buy-modal]'),
-	};
-
-	refs.openModalBtn.addEventListener('click', toggleModal);
-	refs.closeModalBtn.addEventListener('click', toggleModal);
-
-	function toggleModal() {
-		refs.modal.classList.toggle('is-hidden');
-	}
-})();
